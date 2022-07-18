47dcf91541097b37970579afad983acadedfbd31
maoratar <maorizu@gmail.com>
2022-07-18 16:35:35 +0300

    Solved Choosing Matrics ex

--- ComputerVision/TheMachineLearningWorkflow/ChoosingMatrics/precision_recall.py
+++ ComputerVision/TheMachineLearningWorkflow/ChoosingMatrics/precision_recall.py
@@ -1,12 +1,9 @@
 import numpy as np
 
-from iou import calculate_ious
-from utils import get_data
-
 
 def precision_recall(ious, gt_classes, pred_classes):
     """
-    calculate precision and recall
+    calculate precision and recall with using 0.5 as the iou threshold
     args:
     - ious [array]: NxM array of ious
     - gt_classes [array]: 1xN array of ground truth classes
@@ -15,23 +12,22 @@ def precision_recall(ious, gt_classes, pred_classes):
     - precision [float]
     - recall [float]
     """
-    # IMPLEMENT THIS FUNCTION
+    IOU_THERSHOLD = 0.5
+    TP = 0
+    FP = 0
+    FN = 0
+
+    xs, ys = np.where(ious > IOU_THERSHOLD)
+
+    for x, y in zip(xs, ys):
+        if pred_classes[x] == gt_classes[y]:
+            TP += 1
+        else:
+            FP += 1
+
+    FN = len(gt_classes) - len(np.unique(xs))
+
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+
     return precision, recall
-
-
-if __name__ == "__main__": 
-    ground_truth, predictions = get_data()
-    
-    # get bboxes array
-    filename = 'segment-1231623110026745648_480_000_500_000_with_camera_labels_38.png'
-    gt_bboxes = [g['boxes'] for g in ground_truth if g['filename'] == filename][0]
-    gt_bboxes = np.array(gt_bboxes)
-    gt_classes = [g['classes'] for g in ground_truth if g['filename'] == filename][0]
-    
-
-    pred_bboxes = [p['boxes'] for p in predictions if p['filename'] == filename][0]
-    pred_boxes = np.array(pred_bboxes)
-    pred_classes = [p['classes'] for p in predictions if p['filename'] == filename][0]
-    
-    ious = calculate_ious(gt_bboxes, pred_boxes)
-    precision, recall = precision_recall(ious, gt_classes, pred_classes)
