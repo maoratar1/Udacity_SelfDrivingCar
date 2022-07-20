7003b9d6a227952e5cc879910ff82498a1bd0651
maoratar <maorizu@gmail.com>
2022-07-20 12:29:43 +0300

    Added Tensor notebook

--- ComputerVision/SensorAndCameraCalibration/Ex3_GeometricTrans/augmentations.py
+++ ComputerVision/SensorAndCameraCalibration/Ex3_GeometricTrans/augmentations.py
@@ -2,6 +2,8 @@ import copy
 
 import numpy as np 
 from PIL import Image
+import json
+import utils
 
 
 def calculate_iou(gt_bbox, pred_bbox):
@@ -37,7 +39,15 @@ def hflip(img, bboxes):
     - flipped_img [PIL.Image]: horizontally flipped image
     - flipped_bboxes [list[list]]: horizontally flipped bboxes
     """
-    # IMPLEMENT THIS FUNCTION
+    # flip image
+    flipped_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+    w, h = img.size
+
+    # flip bboxes
+    bboxes = np.array(bboxes)
+    flipped_bboxes = copy.copy(bboxes)
+    flipped_bboxes[:, 1] = w - bboxes[:, 3]
+    flipped_bboxes[:, 3] = w - bboxes[:, 1]
     return flipped_img, flipped_bboxes
 
 
@@ -53,6 +63,7 @@ def resize(img, boxes, size):
     - resized_boxes [list[list]]: resized bboxes
     """
     # IMPLEMENT THIS FUNCTION
+    resized_image, resized_boxes = None, None
     return resized_image, resized_boxes
 
 
@@ -69,14 +80,28 @@ def random_crop(img, boxes, crop_size, min_area=100):
     - cropped_boxes [list[list]]: resized bboxes
     """
     # IMPLEMENT THIS FUNCTION
+    cropped_image, cropped_boxes = None, None
     return cropped_image, cropped_boxes
 
-if __name__ == '__main__":
+
+if __name__ == "__main__":
     # fix seed to check results
-    
+    np.random.seed(0)
+
     # open annotations
-    
+    absolute_path = "/Users/maoratar/PycharmProjects/Udacity_SelfDrivingCar/ComputerVision/SensorAndCameraCalibration/Ex3_GeometricTrans/"
+    with open(absolute_path + "data/ground_truth.json") as f:
+        ground_truth = json.load(f)
+
     # filter annotations and open image
-    
+    filename = 'segment-12208410199966712301_4480_000_4500_000_with_camera_labels_79.png'
+    gt_boxes = [g['boxes'] for g in ground_truth if g['filename'] == filename][0]
+    gt_classes = [g['classes'] for g in ground_truth if g['filename'] == filename][0]
+    img = Image.open(absolute_path + f'data/images/{filename}')
+
     # check horizontal flip, resize and random crop
+    flipped_img, flipped_bboxes = hflip(img, gt_boxes)
+    utils.display_results(img, gt_boxes, flipped_img, flipped_bboxes)
+    utils.check_results(flipped_img, flipped_bboxes, aug_type='hflip')
+
     # use check_results defined in utils.py for this
